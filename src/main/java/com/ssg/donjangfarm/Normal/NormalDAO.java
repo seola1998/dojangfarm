7e6aa52e0bb34faf27cb10a76f33cb5f39439585
innjie <innjie@naver.com>
2020-04-21 23:52:30 +0900

    NormalDAO NormalDomain test push

---
+++ src/main/java/com/ssg/donjangfarm/Normal/NormalDAO.java
@@ -0,0 +1,17 @@
+package com.ssg.donjangfarm.Normal;
+
+import java.util.List;
+
+import comssg.dojangfarm.domain.Normal;
+
+public interface NormalDAO {
+	//Normal() throws DataAccessException;
+	void insertSale(Normal normal)  ;
+	void upddateSale(Normal normal);
+	Normal getSale(int saleNo) ;
+	List<Normal> getAllNormalList();
+	List<Normal> getNormalListByUserNo(int userNo);
+	int turnNormalOpen(int saleNo);
+	int turnNormalClosed(int sale) ;
+	List<Normal> searchNormal(String title);// throws DataAccessException;
+}
