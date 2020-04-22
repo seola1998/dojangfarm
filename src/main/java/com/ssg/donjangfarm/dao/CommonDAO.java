60619911eee7b70edcaab312786ca51aa7db9fa8
innjie <innjie@naver.com>
2020-04-22 13:53:40 +0900

    name Refactor

---
+++ src/main/java/com/ssg/donjangfarm/dao/CommonDAO.java
@@ -0,0 +1,16 @@
+package com.ssg.donjangfarm.dao;
+
+import java.util.List;
+
+public interface CommonDAO {
+	//Common() throws dataAcception;
+	void insertSale(CommonDAO common);// throws dataAcception;
+	void upddateSale(CommonDAO common);// throws dataAcception;
+	CommonDAO getSale(int saleNo);// throws dataAcception
+	List<CommonDAO> getAlCommonList();// throws dataAcception;
+	List<CommonDAO> getCommonByUserNo(int userNo);// throws dataAcception;
+	int turnCommonOpen(int saleNo);// throws dataAcception;
+	int turnCommonClosed(int saleNo);// throws dataAcception; //매개변수
+	List<CommonDAO> searchCommon(String title);// throws dataAcception;
+
+}
