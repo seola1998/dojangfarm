553e982802030a4a9cf87f476a8c6f2f06a8c5ca
innjie <innjie@naver.com>
2020-04-22 15:00:27 +0900

    CommonNotice Domain and DAO

---
+++ src/main/java/com/ssg/donjangfarm/dao/CommonNoticeDAO.java
@@ -0,0 +1,7 @@
+package com.ssg.donjangfarm.dao;
+
+public interface CommonNoticeDAO {
+	void insertCommonNotice();// throws dataAcception;
+	void updateCommonNotice(int CNNo);// throws dataAcception;
+	void deletecommonNotice(int CNNo);// throws dataAcception;
+}
---
+++ src/main/java/comssg/dojangfarm/domain/CommonNotice.java
@@ -0,0 +1,28 @@
+package comssg.dojangfarm.domain;
+
+public class CommonNotice {
+	private int CNNo;
+	private String CNTitle;
+	private String CNinfo;
+	
+	public int getCNNo() {
+		return CNNo;
+	}
+	public void setCNNo(int cNNo) {
+		CNNo = cNNo;
+	}
+	public String getCNTitle() {
+		return CNTitle;
+	}
+	public void setCNTitle(String cNTitle) {
+		CNTitle = cNTitle;
+	}
+	public String getCNinfo() {
+		return CNinfo;
+	}
+	public void setCNinfo(String cNinfo) {
+		CNinfo = cNinfo;
+	}
+
+	
+}
